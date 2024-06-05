bad2f1c08cccb2f958a1e6217df6dd03fa413e2e
unknown <Administrator@mca21.labs-psgcas.com>
2024-06-05 13:03:19 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [sequence, setSequence] = useState([]);
 
-function App() {
+  const generateFibonacci = (n) => {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib.slice(0, n);
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setNum(value);
+    setSequence(generateFibonacci(value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>FIBONNACI</h1>
+      <input
+        type="number"
+        value={num}
+        onChange={handleChange}
+        min="0"
+      />
+      <ul className='list'>
+        {sequence.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
